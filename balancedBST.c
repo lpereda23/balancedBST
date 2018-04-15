6f387c1f5dbe7f1441cd4e12b2449bf01cfae0a2
Leo <leonardopereda23@gmail.com>
2018-04-14 21:35:27 -0600

    Added the part of the algorithm that checks if the tree is balanced. Starts at the root node, then if the data is less than the value key go to the left otherwise if it's bigger go to the right.

--- balancedBST.c
+++ balancedBST.c
@@ -1,8 +1,49 @@
 /** 
  * This will be one of the algorithms for the final project.
- * This will be an implementation of a balanced binary search tree.
+ * This will be an implementation of a balanced binary search tree.[]
 */
 #include "stdio.h"
+
+/**
+ * Defining a node having some data, has a reference to both right and left childs
+ */
+struct node 
+{
+    int data;
+    struct node *leftChild;
+    struct node *rightChild;
+};
+
+struct node* search(int data)
+{
+    struct node *current = root;
+    printf("Searching elements: ");
+
+    while(current->data != data)
+    {
+        if(current != NULL)
+        {
+            printf("%d ", current->datat);
+
+            //going to left tree
+            if(current->data > data)
+            {
+                current = current->leftChild;
+            }
+            //else going to right tree
+            else
+            {
+                current = current->rightChild;
+            }
+            //else if current cannot be found
+            if(current == NULL)
+            {
+                return NULL;
+            }
+        }
+    }
+    return current;
+}
 
 int main(void)
 {
